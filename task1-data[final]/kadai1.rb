ec8f3a9e005da72cd82c1a53153c1ef505fd18f0
eggplants <42153744+eggplants@users.noreply.github.com>
2019-01-19 16:46:40 +0900

    Update kadai1.rb
    remove comments

--- task1-data[final]/kadai1.rb
+++ task1-data[final]/kadai1.rb
@@ -1,68 +1,60 @@
 ###first, load redirected txt
-str_0 = $stdin.read.to_s.delete('"')#receive txt, delete quotation and save at str_0
-###then split into elements in array
-str_1 = str_0.gsub(/\n/, " ")#line break replace space
-.gsub(/\?/, ".").gsub(/!/, ".").gsub(/:/, ".")#sentence-end marks replace period
-.gsub(/'/, "s").gsub(/-/, "h")#strings that treat as one alphabet replace into it
-.delete(",").split(".")#split array str0 into arrays by a sentence
-###split into sentences
-str_2 = []#str_2 is a array to save sentences
-incr = 0#Initialize variable "incr" *this finally show how many sentences*
-#Every sentence split into words and save in str_2
+str_0 = $stdin.read.to_s.delete('"')
+str_1 = str_0.gsub(/\n/, " ").gsub(/(\?|!|:)/, ".")
+.gsub(/'/, "s").gsub(/-/, "h")
+.delete(",").split(".")
+str_2,incr = [],0
 str_1.each {|sentence|
   str_2[incr] = sentence.scan(/\w+/)
   incr += 1
 }
-str_2.delete_at(-1)#delete one blank line to the end of str_2
-incr -= 1#this value equals num. of sentence(s)
-###detect length of president name
+str_2.delete_at(-1)
+incr -= 1
 p_lng_0 = str_0.scan(/^[A-Z]{1}[a-z]+.*\n/)
 p_lng_1 = p_lng_0[0].split.length
+
 ###retrive asitating president name
 president = str_2[0][0, p_lng_1]
-presidentName = president.join(' ')#erements in array changes one string
+presidentName = president.join(' ')
 #goodbye, president
 str_2[0].slice!(0..p_lng_1-1)
-#detect how many paragraphs does this sentenses have
 para_n = str_0.scan(/\n\n/).size + 1
+
 ###average of word(s) per sentence
 sum_w = 0.to_f
-for a in 0..(incr - 1)#sum of num. of word(s) in sentence
+for a in 0..(incr - 1)
   sum_w += str_2[a].size
 end
-avg_w = sum_w / incr#sum devides num. of sentence
+avg_w = sum_w / incr
+
 ###variance of word(s) per sentence
 dev_sq_w = 0.to_f
-for b in 0..(incr - 1)#sum of number that deviate num. of sentence, and then squar
+for b in 0..(incr - 1)
   dev_sq_w += (str_2[b].size - avg_w)** 2
 end
-var_w = dev_sq_w / incr#sum devides num. of sentence
+var_w = dev_sq_w / incr
+
 ###split array str0 into arrays of entire word(s) in str_3(array)
-#strings that treat as one alphabet replace into it, and then split into words
 str_3 = str_0.downcase.gsub(/'/, "s").gsub(/-/, "h").scan(/\w+/)
-str_3.slice!(0, p_lng_1)# delete president name
+str_3.slice!(0, p_lng_1)
+
 ###avg. of entire word(s)
 avg_e = 0.to_f
-for c in 0..(str_3.size - 1)#sum of length of word
+for c in 0..(str_3.size - 1)
   avg_e += str_3[c].length
 end
-avg_e = avg_e / str_3.size#sum devides num. of word
+avg_e = avg_e / str_3.size
+
 ###var. of entire word(s)
 dev_sq_e = 0.to_f
-for d in 0..(str_3.size - 1)#sum of number that deviate num. of word and then squar
+for d in 0..(str_3.size - 1)
   dev_sq_e += (str_3[d].size - avg_e) ** 2
 end
-var_e = dev_sq_e / str_3.sizen‚†#sum devides num. of word
+var_e = dev_sq_e / str_3.size
+
 ###entire word(s)
-word_n = str_3.uniq.size#num. of entire word(s)
+word_n = str_3.uniq.size
+
 #export results
-print(presidentName, ",",#president name
-   para_n, ",",#num. of entire paragraph(s)
-    incr, ",",#num. of entire sentence(s)
-    "%.2f" % avg_w, ",",#avg. of word(s) per sentence
-     "%.2f" % var_w, ",",#var. of word(s) per sentence
-      "%.2f" % avg_e, ",",#avg. of entire word(s)
-       "%.2f" % var_e, ",",#var. of entire word(s)
-        word_n,#num. of entire word(s)
-          "\n"
-      )
+puts %W(#{presidentName} #{para_n,incr} #{"%.2f" % avg_w}
+#{"%.2f" % var_w} #{"%.2f" % avg_e} #{"%.2f" % var_e} #{word_n}).join(",")
